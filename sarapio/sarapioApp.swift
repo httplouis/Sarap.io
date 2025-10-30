00467b205fc5188c806e602e188e09ba865e24ca
STUDENT <student@STUDENTs-iMac.local>
2025-10-30 18:06:49 +0800

    isa

--- sarapio/sarapioApp.swift
+++ sarapio/sarapioApp.swift
@@ -1,46 +1,32 @@
+//
+//  SarapioApp.swift
+//  Sarapio
+//
+//  Created by STUDENT on 10/30/25.
+//
+
 import SwiftUI
+import SwiftData
 
 @main
-struct SarapIOApp: App {
-    @StateObject private var store = RecipeStore()
-    @StateObject private var session = SessionManager()
-    @StateObject private var feedStore = SocialFeedStore()
+struct SarapioApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
 
-    init() {
-        let appearance = UINavigationBarAppearance()
-        appearance.configureWithOpaqueBackground()
-        appearance.backgroundColor = UIColor(Theme.bg)
-        appearance.titleTextAttributes = [.foregroundColor: UIColor(Theme.text)]
-        appearance.largeTitleTextAttributes = [.foregroundColor: UIColor(Theme.text)]
-
-        UINavigationBar.appearance().standardAppearance = appearance
-        UINavigationBar.appearance().scrollEdgeAppearance = appearance
-        UINavigationBar.appearance().compactAppearance = appearance
-        UINavigationBar.appearance().tintColor = UIColor(Theme.olive)
-
-        let tabBarAppearance = UITabBarAppearance()
-        tabBarAppearance.configureWithOpaqueBackground()
-        tabBarAppearance.backgroundColor = UIColor(Theme.bg)
-        UITabBar.appearance().standardAppearance = tabBarAppearance
-        UITabBar.appearance().scrollEdgeAppearance = tabBarAppearance
-    }
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
 
     var body: some Scene {
         WindowGroup {
-            Group {
-                if session.currentUser == nil {
-                    AuthView()
-                        .environmentObject(session)
-                } else {
-                    ContentView()
-                        .environmentObject(store)
-                        .environmentObject(session)
-                        .environmentObject(feedStore)
-                }
-            }
-            .tint(Theme.olive)
-            .background(Theme.bg.ignoresSafeArea())
-            .preferredColorScheme(.light)
+            ContentView()
         }
+        .modelContainer(sharedModelContainer)
     }
 }
